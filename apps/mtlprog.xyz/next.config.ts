d745b6826d2cc97194faf2405c5ad2f763930cc0
Stanislav Karkavin <me@xdefrag.dev>
2025-10-02 16:20:50 +0200

    feat(mtlprog.xyz): add stellar.toml

--- apps/mtlprog.xyz/next.config.ts
+++ apps/mtlprog.xyz/next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/stellar.toml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain; charset=utf-8",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
